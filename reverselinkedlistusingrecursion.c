6065bc1688ce96444b4de58903f8f0accce15e15
Gunank <gunanksood1222@gmail.com>
2017-02-08 19:39:10 +0530

    c program to reverse a linked list

---
+++ reverselinkedlistusingrecursion.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node* next;
+
+};
+struct node* head;
+struct node* create_ll(int d)
+{
+    struct node* ins = malloc(sizeof(struct node));
+    ins->data = d;
+    if(head == NULL)
+    {
+        head=ins;
+        ins->next = NULL;
+    }
+    else
+    {
+        struct node* temp = head;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        ins->next = NULL;
+        temp->next = ins;
+
+    }
+    return head;
+}
+
+void print()
+{
+    struct node* temp = head;
+    while(temp != NULL)
+    {
+        printf("%d  ",temp->data);
+        temp = temp->next;
+
+    }
+printf("\n");
+
+}
+
+struct node* reverse(struct node* head)
+{
+    if(head == NULL)
+        return NULL;
+    struct node* new_head;
+    if(head->next == NULL)
+        {
+            new_head = head;
+            return new_head;
+        }
+    new_head = reverse( head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return new_head;
+}
+
+int main(void)
+{
+    int n,i;
+
+    printf("enter the no. of nodes");
+    scanf("%d",&n);
+    head=NULL;
+    for(i = 0; i < n; i++)
+    {
+        int d;
+        scanf("%d",&d);
+        head = create_ll(d);
+    }
+    printf("linked list is : ");
+    print();
+    head = reverse(head);
+    printf("\nafter reverse linked list is : ");
+    print();
+    return 0;
+}
