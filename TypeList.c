cf7a49368f96a0d21ffb704597f5f72fe9cd3888
ChatN0ir <lacostejustin2@gmail.com>
2019-10-14 18:04:42 +0200

    Added comments, refactored code, fixed some bugs

--- TypeList.c
+++ TypeList.c
@@ -3,174 +3,210 @@
 #include <string.h>
 
 
-typedef struct tlist {
-    size_t typeSize;
+typedef struct _TypeList TypeList;
+
+struct _TypeList {
     size_t size;
+    size_t _typeSize;
     size_t _iterator;
 
-    void (*push)(struct tlist**, char*);
-    char* (*next)(struct tlist**);
-    void (*pop)(struct tlist**, char*);
-    void (*shift)(struct tlist**, char*);
-    int (*rewind)(struct tlist**);
+    void(*push)(TypeList**, char*);
+    char*(*get)(TypeList**, int);
+    void(*remove)(TypeList**, int);
+    char*(*next)(TypeList**);
+    void(*rewind)(TypeList**);
+    void(*pop)(TypeList**, char**);
+    void(*shift)(TypeList**, char**);
+    
 
-    char* addressSpace;
 
-} TypeList;
+    char* space;
+};
 
-char* _next(TypeList** l){
-    if((*l)->_iterator < (*l)->size){
-        (*l)->_iterator++;
-        return ((char*) (*l)->addressSpace + ((*l)->_iterator * (*l)->typeSize));
+char* _next(TypeList** list){
+    TypeList* l = (*list);
+    if(l->_iterator < l->size){
+        l->_iterator++;
+        return l->get(list, l->_iterator - 1);
     }else{
         return NULL;
     }
 }
-
-// C calls function __by value__, we need a Pointer to a Pointer in order to change its address (**int x -> *(x) = 0xDEADC0DE)
-void _push(TypeList** l, char* b){
-    // Allocate new Memory
-    TypeList** oldpointer = l;
-    TypeList* lPointer = (TypeList*) realloc(*l, sizeof(TypeList) + ((*l)->size+1) * (*l)->typeSize);
-    if(lPointer == NULL) return;
-    *l = lPointer;
-
-    // Resize Actual List
-    char* accessSpacePointer = (char*) realloc((*l)->addressSpace, (((*l)->size + 1) * (*l)->typeSize));
-    if(accessSpacePointer == NULL) return;
-    (*l)->addressSpace = (char*) accessSpacePointer;
-
-    memcpy((void*) ((*l)->addressSpace + (((*l)->size+1) * (*l)->typeSize)), (void*) b, (*l)->typeSize);
-
-    (*l)->size++;
+void _rewind(TypeList** list){
+    (*list)->_iterator = 0;
     return;
 }
 
-void _pop(TypeList **l, char* buffer){
-    for(int i = 0; i <= (*l)->size; i++){
-        buffer[i] = *((*l)->addressSpace + ((((*l)->size) * ((*l)->typeSize)) + i));
-    }
+void _push(TypeList** list, char* data){
+    // Dereference Double Pointer (Pointer -> Pointer -> Actual Object) to (Pointer -> Actual Object)
+    TypeList* l = (*list);
 
-    void* lPointer = realloc((*l), sizeof(TypeList) + (((*l)->size-1) * (*l)->typeSize));
-    if(lPointer == NULL) return;
-    *l = lPointer;
-
-
-    lPointer = realloc((*l)->addressSpace, (((*l)->size - 1) * (*l)->typeSize));
-    if(lPointer == NULL) return;
-    (*l)->addressSpace = (char*) lPointer;
-
-    (*l)->size--;
-
-
-}
-
-void _shift(TypeList** l, char* buffer){
-
-    printf("0-1: %p \n0-2: %p\n", 
-        (*l)->addressSpace, ((char*) (*l)->addressSpace + ((*l)->_iterator * (*l)->typeSize)), 
-        (int*)((char*) (*l)->addressSpace + ((*l)->_iterator * (*l)->typeSize))
-    
-    );
-
-    // Put 1st element into char array <buffer>, i know this is spaghetti code, even i dont understand what is happening here.... but it works
-    for(int i = (*l)->typeSize; i < (*l)->typeSize * 2; i++){
-        buffer[i - (*l)->typeSize] = *(int*)((char*) (*l)->addressSpace + ((*l)->typeSize + (i - (*l)->typeSize)));
-    }
-
-    // Move all elements to n-1
-    for(int i = 2; i < (*l)->size+1; ++i){
-        memcpy((*l)->addressSpace + ((i-1) * (*l)->typeSize), (*l)->addressSpace + (i * (*l)->typeSize), (*l)->typeSize);
-    }
-    (*l)->size--;
-
-
-    // Reallocate struct
-    void* sPointer = realloc((*l), sizeof(TypeList) + (((*l)->size) * (*l)->typeSize));
-    if(sPointer == NULL) return;
-    *l = sPointer;
-
-    // Reallocate list
-    char* newPointer = realloc((*l)->addressSpace, (*l)->size * (*l)->typeSize);
+    // Allocate already claimed memory + the actual size of the list (size+1 * typeSize)
+    TypeList* newPointer = (TypeList*) realloc(l, sizeof(*l) + (l->size+1 * l->_typeSize));
     if(newPointer == NULL) return;
-    (*l)->addressSpace = newPointer;
+    (*list) = newPointer;
+    l = newPointer;
+
+    char* newList = (char*) realloc(l->space, sizeof(l->space + (l->size + 1 * l->_typeSize)));
+    if(newList == NULL) return;
+    l->space = newList;
+
+    // Copy <data> to the end of the list
+    // End of the list = Address of list + (List Size * Type)
+    
+    // f.e. 0x7FFFD403A664 + (6 * 4) = 0x7FFFD403A67C
+    // The new Element will be located @ 0x7FFFD403A67C
+    memcpy((l->space + (l->size * l->_typeSize)), data, l->_typeSize);
+    l->size++;
     return;
 }
 
-int _rewind(TypeList** l){
-    int size = (*l)->_iterator;
-    (*l)->_iterator = 0;
-    return size;
+char* _get(TypeList** list, int index){
+    TypeList* l = (*list);
+    return l->space + (index * l->_typeSize);
 }
 
-TypeList* List(size_t type) {
-    TypeList *list = (TypeList*) malloc(sizeof(TypeList));
-    list->typeSize = type;
-    list->size = 0;
-    list->_iterator = 0;
-    list->push = &_push;
-    list->next = &_next;
-    list->pop = &_pop;
-    list->shift = &_shift;
+void _remove(TypeList** list, int index){
+    TypeList* l = (*list);
+    
+    // Set specified index to 0 if in range
+    if(index < l->size){
+        memset(l->space + (index * l->_typeSize), '\0', l->_typeSize);
 
-    list->rewind = &_rewind;
+        // Iterate over all elements after index and memcpy them to the memory address before them
+        for(int i = index+1; i < l->size; ++i){
+            memcpy((l->space + ((i-1) * l->_typeSize)), (l->space + (i * l->_typeSize)), l->_typeSize);
+        }
+        // Decrement the List size
+        l->size--;
 
-    list->addressSpace = (char*) malloc(sizeof(char));
-    return list;
+
+        // Allocate more memory for entire struct
+        TypeList* listPointer = (TypeList*) realloc(*list, sizeof(TypeList) + (l->size * l->_typeSize));
+        if(listPointer == NULL) return;
+        list = &listPointer;
+
+        // Re-allocate memory for actual char Array (List)
+        char* newPointer = (char*) realloc(l->space, (l->size * l->_typeSize));
+        if(newPointer == NULL) return;
+        l->space = newPointer;
+
+        // This is done to ensure, that when other parts of the Program allocate memory on the heap these Objects dont overlap with out list and accidentaly overwrite the list content
+    }
+}
+
+void _pop(TypeList** list, char** buffer) {
+    TypeList* l = (*list);
+    printf("Copying %liB from %p -> %p\n", l->_typeSize, l->get(list, l->size-1), *buffer);
+    memcpy(*buffer, l->get(list, l->size-1), l->_typeSize);
+    l->remove(list, l->size-1);
+    return;
+}
+
+void _shift(TypeList** list, char** buffer){
+    TypeList* l = (*list);
+    // Copy the content of the first element into the buffer
+    memcpy(*buffer, l->get(list, 0), l->_typeSize);
+    // Remove the first element and then move all following elements to n-1
+    l->remove(list, 0);
+    return;
 }
 
 
+TypeList* List(size_t typeSize){
+    TypeList* a = (TypeList*) malloc(sizeof(TypeList));
+    a->_typeSize = typeSize;
+
+    /* Functions */
+    a->push = &_push;
+    a->get = &_get;
+    a->next = &_next;
+    a->remove = &_remove;
+    a->rewind = &_rewind;
+    a->pop = &_pop;
+    a->shift = &_shift;
+
+    /* Allocation of actual list */
+    a->space = (char*) malloc(sizeof(char*));
+    return a;
+}
 
 
 int main(int argc, char** argv){
-    TypeList* a = List(sizeof(int));
-    int val = 8831;
-    int value2 = 994122;
-    int value3 = 391027;
-    a->push(&a, (char*) &val);
-    a->push(&a, (char*) &value2);
-    a->push(&a, (char*) &value3);
-    
-    printf("List is now @%p\nLength: %li\n", a, a->size);
 
-    char buffer[4] = {'\0', '\0', '\0', '\0'};
-    a->pop(&a, (char*) buffer);
-    printf("Popped of Value: %i\n", *((int*) buffer));
+    // Please note, that i'm not an expert at C so this may or may not be a bit inaccurate
+
+    // Create a new TypeList* with Integers as elements
+    TypeList* t = List(sizeof(int));
+    // TypeList::push as well as every other TypeList::* Function takes a double pointer to the actual list as the very first parameter
+    // The second parameter needs to be declared beforehand because of the way the TypeList works:
+    // TypeList takes the char* and copies TypeList::_typeSize Bytes of it to the end of the list
+    // TypeList can be compared to the Java ArrayList<Type>();
+    
+    // Example on integers:
+    // int a = 44;
+    // TypeList::push(&List, (char*) &a)
+    // Can be abbreviated to:
+    // TypeList::push(&List, &a); {throws a warning that can be ignored}
+    int data = 41284124;
+    t->push(&t, (char*) &data);
+    int data2 = 0x847;
+    t->push(&t, (char*) &data2);
 
     int* i;
-    while((i = (int*) a->next(&a)) != NULL){
-        printf("[@%p]-> %i\n", i, *i);
-    }
-
-    char puffer[4] = {0x00, 0x00, 0x00, 0x00};
-    a->shift(&a, (char*)puffer);
-    printf("Shifted value: %i\n", *((int*) puffer));
-
-    printf("Rewinding, internal counter was @%i\n", a->rewind(&a));
-    
-    int* x;
-    while((x = (int*) a->next(&a)) != NULL){
-        printf("[@%p]-> %i\n", x, *x);
-    }
-
-    printf("--- Initializing a second list ---\n");
-    TypeList* newList = List(sizeof(int));
-    int c = 4214124;
-    int k = 53425;
-    int l = 523875;
-    newList->push(&newList, &c);
-    newList->push(&newList, &k);
-    newList->push(&newList, &l);
-
-
-
-    int* o;
     int indx = 0;
-    while((o = (int*) newList->next(&newList)) != NULL){
-        printf("NewList[%i] = %i\n", indx, *o);
+    // TypeList::next returns the char* Pointer to the next element in the list, each call increments the lists internal _iterator object
+    while((i = (int*) t->next(&t)) != NULL){
+        printf("List[%i] => %i\n", indx, *i);
+        indx++;
+    }
+    // TypeList::remove, well... removes the specified index from the list
+    t->remove(&t, 0);
+    // Rewind resets the list's internal _iterator object to 0 so the TypeList::next call would return the first index
+    t->rewind(&t);
+    printf("Remove List[0], rewinded: \n");
+
+    indx = 0;
+    while((i = (int*) t->next(&t)) != NULL){
+        printf("List[%i] => %i {%p}\n", indx, *i, i);
         indx++;
     }
 
-    return 0;
+    printf("Adding 3 new Elements to the %li element big list\n", t->size);
+    int a = 485125;
+    int b = 71805617;
+    int c = 578;
+    t->push(&t, (char*) &a);
+    t->push(&t, (char*) &b);
+    t->push(&t, (char*) &c);
 
+    char* buffer = malloc(t->_typeSize);
+    // pop expects a char** buffer as it's second argument, this has to be malloc'ed so the function can access the array on the <HEAP>, it then memcpy's <N> Bytes from the end of the list into the buffer and removes
+    // the element from list 
+    t->pop(&t, &buffer);
+    printf("Popped of %i from the end of the list\n", *(int*)buffer);
+
+    
+    t->rewind(&t);
+    indx = 0;
+    while((i = (int*) t->next(&t)) != NULL){
+        printf("List[%i] => %i\n", indx, *i);
+        indx++;
+    }
+
+    // Shift removes the first element of the list, it works exactly the same as TypeList::pop
+    t->shift(&t, &buffer);
+    printf("Shifted %i from the start of the list\n", *(int*)buffer);
+    t->rewind(&t);
+    indx = 0;
+    while((i = (int*) t->next(&t)) != NULL){
+        printf("List[%i] => %i\n", indx, *i);
+        indx++;
+    }
+
+    free(buffer);
+    free(t);
+
+
+    return 0;
 }
